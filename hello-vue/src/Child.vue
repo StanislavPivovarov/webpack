dcfce5046c705dc3555f5a17ef2a6a1f0742b3d5
stanislav <pivovarov@bluefountainmedia.com>
2017-01-20 19:27:12 +0200

    vue

---
+++ hello-vue/src/Child.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        {{mainText}}
+    </div>
+</template>
+<style>
+    body{
+        background-color:#000;
+    }
+</style>
+<script>
+    export default{
+        props: ['childParam'],
+        computed :
+        {
+           mainText: function () {
+            return this.childParam
+            //@TODO NOT WORK WHY () => this.childParam
+           }
+        }
+    }
+</script>
---
+++ hello-vue/src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Bar from './Bar.vue'
+import VueRouter from 'vue-router'
+
+const routes = [
+    { path: '/', component: App },
+    { path: '/bar', component: Bar }
+];
+
+const router = new VueRouter({routes});
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app');
